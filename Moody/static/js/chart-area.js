65593fbffc8c858feb7487b35ce36958c59cb2cc
Nikolay Pomytkin <nikolayp2800@gmail.com>
2019-03-09 22:29:49 -0500

    finished with static changes to dashboard

--- Moody/static/js/chart-area.js
+++ Moody/static/js/chart-area.js
@@ -78,7 +78,7 @@ var myLineChart = new Chart(ctx, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return  number_format(value);
           }
         },
         gridLines: {
