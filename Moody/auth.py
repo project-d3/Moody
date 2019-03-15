fa4fb431c0a209735fb6da1520747f31c21a4916
ryanmao725 <ryan.mao.725@gmail.com>
2019-03-15 10:03:26 -0400

    added beautiful forgot password screen

--- Moody/auth.py
+++ Moody/auth.py
@@ -111,3 +111,7 @@ def logout():
     """Clear the current session, including the stored user id."""
     session.clear()
     return redirect(url_for('index'))
+
+@bp.route('/forgotpass')
+def forgotpass():
+    return render_template('auth/forgotpass.html')
